8df3c14f05216cd5b9c9cd55bc4ef29c6c49737c
Richard Spencer <contact@spencerdeals.bm>
2025-10-19 18:29:42 -0300

    Delete backend directory

--- backend/routes/version.js
+++
@@ -1,7 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const version = require('../serverVersion');
-
-router.get('/', (req, res) => res.json(version));
-
-module.exports = router;
